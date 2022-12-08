e22eb23e35066005a04a1e0c477e723b4667c7ff
Yuqian He <taabbyy.0525@gmail.com>
2022-12-08 19:39:22 +0000

    update blankNGL

---
+++ PBDNGL/README.md
@@ -0,0 +1,27 @@
+# BlankNGL
+
+![alt tag](http://nccastaff.bournemouth.ac.uk/jmacey/GraphicsLib/Demos/BlankNGL.png)
+
+This is an empty boilerplate framework for NGL projects, it creates an empty window and draws nothing.
+
+To build you need NGL installed as per the instructions [here](https://github.com/NCCA/NGL)
+
+Windows
+
+```
+mkdir build
+cd build
+cmake -DCMAKE_PREFIX_PATH=~/NGL ..
+cmake --build .
+
+```
+
+Mac Linux
+
+
+```
+mkdir build
+cd build
+cmake -DCMAKE_PREFIX_PATH=~/NGL ..
+make 
+```
---
+++ PBDNGL/src/NGLScene.cpp
@@ -0,0 +1,74 @@
+#include <QMouseEvent>
+#include <QGuiApplication>
+
+#include "NGLScene.h"
+#include <ngl/NGLInit.h>
+#include <ngl/VAOPrimitives.h>
+#include <iostream>
+
+NGLScene::NGLScene()
+{
+  // re-size the widget to that of the parent (in this case the GLFrame passed in on construction)
+  setTitle("Blank NGL");
+}
+
+
+NGLScene::~NGLScene()
+{
+  std::cout<<"Shutting down NGL, removing VAO's and Shaders\n";
+}
+
+
+
+void NGLScene::resizeGL(int _w , int _h)
+{
+  m_win.width  = static_cast<int>( _w * devicePixelRatio() );
+  m_win.height = static_cast<int>( _h * devicePixelRatio() );
+}
+
+
+void NGLScene::initializeGL()
+{
+  // we must call that first before any other GL commands to load and link the
+  // gl commands from the lib, if that is not done program will crash
+  ngl::NGLInit::initialize();
+  glClearColor(0.7f, 0.7f, 0.7f, 1.0f);			   // Grey Background
+  // enable depth testing for drawing
+  glEnable(GL_DEPTH_TEST);
+  // enable multisampling for smoother drawing
+  glEnable(GL_MULTISAMPLE);
+
+}
+
+
+
+void NGLScene::paintGL()
+{
+  // clear the screen and depth buffer
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glViewport(0,0,m_win.width,m_win.height);
+
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+void NGLScene::keyPressEvent(QKeyEvent *_event)
+{
+  // this method is called every time the main window recives a key event.
+  // we then switch on the key value and set the camera in the GLWindow
+  switch (_event->key())
+  {
+  // escape key to quite
+  case Qt::Key_Escape : QGuiApplication::exit(EXIT_SUCCESS); break;
+  case Qt::Key_Space :
+      m_win.spinXFace=0;
+      m_win.spinYFace=0;
+      m_modelPos.set(ngl::Vec3::zero());
+
+  break;
+  default : break;
+  }
+  // finally update the GLWindow and re-draw
+
+    update();
+}
